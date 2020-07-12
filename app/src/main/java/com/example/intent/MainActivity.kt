ad0d5d9cc267388ddddae3f9e931d6dc00162479
yuyaprograming <ienjoyschoollife@gmail.com>
2020-07-12 22:44:25 +0900

    Initial commit

---
+++ app/src/main/java/com/example/intent/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.intent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
+    }
+}
